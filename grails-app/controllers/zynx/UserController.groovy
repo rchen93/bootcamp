01d9ba9aa44015c2e8b7bcf6a38703c94cadac0e
rchen93 <rchen93@ucla.edu>
2014-07-28 01:41:34 -0700

    Added advanced search

--- grails-app/controllers/zynx/UserController.groovy
+++ grails-app/controllers/zynx/UserController.groovy
@@ -12,4 +12,27 @@ class UserController {
     	return [ users: users, term: params.userId,
     			totalUsers: User.count()]
     }
+
+    def advSearch() {}
+
+    def advResults() {
+        def profileProps = Profile.metaClass.properties*.name
+        def profiles = Profile.withCriteria {
+            "${params.queryType}" {
+
+                params.each { field, value ->
+
+                    if (profileProps.grep(field) && value) {
+                        ilike(field, value)
+                    }
+                }
+
+            }
+
+        }
+        [ profiles : profiles ]
+
+    }
 }
+
+
