77c791078b0a1dc2ecfbef694ab2d2360f98eef2
rchen93 <rchen93@ucla.edu>
2014-07-28 01:58:12 -0700

    Added timeline and PostController unit tests

--- grails-app/controllers/zynx/PostController.groovy
+++ grails-app/controllers/zynx/PostController.groovy
@@ -2,4 +2,13 @@ package zynx
 
 class PostController {
     static scaffold = true
+
+    def timeline() {
+        def user = User.findByUserId(params.id)
+        if (!user) {
+            response.sendError(404)
+        } else {
+            [ user : user ]
+        }
+    }
 }
--- test/unit/zynx/PostControllerTests.groovy
+++ test/unit/zynx/PostControllerTests.groovy
@@ -1,7 +1,5 @@
 package zynx
 
-
-
 import grails.test.mixin.*
 import org.junit.*
 
@@ -9,9 +7,25 @@ import org.junit.*
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
 @TestFor(PostController)
+@Mock([User, Post])
 class PostControllerTests {
 
-    void testSomething() {
-       fail "Implement me"
+    void testTimelineGivenId() {
+       User chuck = new User(userId: "chuck_norris", password: "password")
+       chuck.addToPosts(new Post(content: "A first post"))
+       chuck.addToPosts(new Post(content: "A second post"))
+       chuck.save(failOnError: true)
+
+       params.id = chuck.userId
+
+       def model = this.controller.timeline()
+       assert model.user.userId == "chuck_norris"
+       assert model.user.posts.size() == 2
+    }
+
+    void testErrorWhenNonExistentUser() {
+    	params.id = "this-user-id-does-not-exist"
+    	this.controller.timeline()
+    	assert response.status == 404
     }
 }
