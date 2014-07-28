aa33ea57460dda84c04d0c2d8e0b5ab8b2ef51d7
rchen93 <rchen93@ucla.edu>
2014-07-28 00:56:05 -0700

    Added user search

--- grails-app/controllers/zynx/UserController.groovy
+++ grails-app/controllers/zynx/UserController.groovy
@@ -2,4 +2,14 @@ package zynx
 
 class UserController {
     static scaffold = true
+
+    def search() {}
+
+    def results(String userId) {
+    	def users = User.where {
+    		userId =~ "%${userId}%"
+    	}.list()
+    	return [ users: users, term: params.userId,
+    			totalUsers: User.count()]
+    }
 }
